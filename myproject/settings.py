7a47a084f6e43c16b268c5e97d2466e2d44cd7a0
Christina Hadi <christina.lamau@gmail.com>
2025-08-19 13:08:14 -0400

    Final project complete: Online Course App with Exam Results

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'onlinecourse/static'),
+]
+
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 
